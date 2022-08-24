186eb44b12695541a9ffd4c4a8b534442963d27f
czrpb <czrpb@yahoo.com>
2022-08-24 08:30:30 -0700

    updates to use cmd-line vars

--- constant/constant.js
+++ constant/constant.js
@@ -3,12 +3,14 @@ import http from 'k6/http';
 
 export const options = {
   stages: [
-    { duration: '20s', target: 1 },
+    { duration: `${__ENV.seconds}s`, target: 1 },
   ]
 };
+
+var every = parseInt(`${__ENV.every}`) - 0.1;
 
 export default () => {
   http.get('https://test.k6.io');
 
-  sleep(2);
+  sleep(every);
 };
